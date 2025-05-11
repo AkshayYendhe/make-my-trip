e46f7e2d35a2407377fde3f2c6d333135c828415
akshay-rnd <akshay.yendhe@rndtechnosoft.in>
2025-05-11 20:14:16 +0530

    sample service added

---
+++ src/main/java/com/example/make_my_trip/sample.java
@@ -0,0 +1,15 @@
+package com.example.make_my_trip;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class sample {
+    public sample() {
+    }
+
+    @GetMapping({"/sample"})
+    public String getData() {
+        return "sample";
+    }
+}
